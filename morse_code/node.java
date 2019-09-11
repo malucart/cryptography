03a5b9caa53a351da2c01a92ada45d00e70f4556
malucart <cartaxo@lcc.ufrn.br>
2019-09-11 00:54:25 -0700

    Update node.java

--- morse_code/node.java
+++ morse_code/node.java
@@ -1,5 +1,4 @@
 /*
-   Maria Luiza Cartaxo Rocha de Sousa
    - Final Project -
    Building Java Programs: A Back to Basics Approach, 5th edition, chapter 17 (Binary Trees), Programming Projects section #2
    CS 211
