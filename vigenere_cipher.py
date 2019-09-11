77e49d3d987168ab0dba2f4c47c47cfc348429c6
malucart <cartaxo@lcc.ufrn.br>
2019-09-11 00:52:12 -0700

    Update vigenere_cipher.py

--- vigenere_cipher.py
+++ vigenere_cipher.py
@@ -5,38 +5,34 @@
   https://en.wikipedia.org/wiki/Vigenère_cipher
 
   In my example, I have a key called ROCKET
-  and I need to decrypt a text (txt_encrypt) according with this key
+  and I need to decrypt a text according with this key
 
-  If I have "S" and the first letter in the key is "R"
-  A = 1, B = 2, C = 3, D = 4 [...]
-  it means that S = 19 and R = 18
+  If I have "S" as the first letter on the text
+  and "R" as the first letter on the key
+  A = 0, B = 1, C = 2, D = 3 [...]
+  it means that S = 18 and R = 17
   so it jumps down 1 letter
-  19 - 18 = 1
-  starting in A, A + 1 = B
+  18 - 17 = 1
+  so starting in A,
+  A + 1 =
+  0 + 1 =
+  = B
   result: with S letter and R key, the real text (plaintext) is B
-
-  Now example with the next letter and key,
-  C instead of S, O instead of R
-  it means that C = 3 and O = 15
-  so it jumps down 14 letters
-  starting in A, A + 14 = O
-  resukt: with C letter and O key, the plaintext is O
 
   and so on.
 
 '''
-#import numpy as np
 
 # hint: start in "if __name__ == '__main__':"
 
-# second method called with two arguments
+# second method called with three arguments
 def letter_jumpdown(letter, position, j):
   print("----------------------")
   # necessary to compare array soon
   alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
   # upper(): responsable to trasform all the letter in upper letter
-  # but it already is upper letter or it is another thing but no a letter, keep on the same character
+  # but it already is upper letter or it is another thing but no a letter, keep the same character
   # example 1:
   # using 'S' as the first letter in txt_encrypt
   # char_ascii = ord('S') - 65
@@ -95,19 +91,19 @@ def letter_jumpdown(letter, position, j):
     # example 1:
     # using 'S' as the first letter in txt_encrypt
     # result = ord('S') - ord('R')
-    # result = 83 - 82
+    # result = (83 - 82) % 26 =
     # result = 1
     #
     # example 2:
     # using 'C' as the second letter in txt_encrypt
     # result = ord('C') - ord('O')
-    # result = 67 - 79
-    # result = -12
+    # result = (67 - 79) % 26
+    # result = 14
     #
     # example 3:
     # using 'Q' as the third letter in txt_encrypt
     # result = ord('Q') - ord('C')
-    # result = 81 - 67
+    # result = (81 - 67) % 26
     # result = 14
     result = (ord(letter.upper()) - ord(position[j]))%26
     print('[-] result: {0}'.format(result))
@@ -117,7 +113,7 @@ def letter_jumpdown(letter, position, j):
     # result = 1, so it stays here
     #
     # example 2:
-    # result = -12, so ignore it and go ahead
+    # result = 14, so it stays here
     #
     # example 3:
     # result = 14, so it stays here
@@ -131,61 +127,48 @@ def letter_jumpdown(letter, position, j):
       # returns B
       #
       # example 2:
-      # it is not here
+      # chr(ord('A') + result)
+      # chr(65 + 14)
+      # chr(79)
+      # 0
+      # returns 0
       #
       # example 3:
       # chr(ord('A') + result)
       # chr(65 + 14)
       # chr(79)
-      # O
-      # return O
+      # 0
+      # returns 0
       final_result_1 = chr(ord('A') + result)
-      print('[-] final_result_1: {0}'.format(final_result_1))
+      print('[-] final result: {0}'.format(final_result_1))
       return final_result_1
     else:
-      # example 2 stays here
-      for i in alphabet:
-
-        # if i == 'C' (in some moment it will be)
-        if i == letter.upper():
-
-            # result2 = C - (-12)
-            # result2 = O // this is the result!!
-            final_result_3 = (alphabet.index(i) - result);
-            final_result_2 = alphabet[final_result_3]
-
-            print("[-] final_result_3: {0}".format(final_result_3))
-            print("[-] alphabet.index(i) value: {0}".format(alphabet.index(i)))
-            print("[-] result value: {0}".format(result))
-            print("[-] final_result_2: {0}".format(final_result_2))
-            return final_result_2
+      print('[-] sorry, something is wrong')
 
 # first method called without argument
 def main():
 
-  # txt_encrypt is the string that has cryptography, it means we dont understand it
+  # txt_encrypt is the string that has cryptography, it means we dont understand the text
   txt_encrypt = "SCQ CHL HYYGU OO"
   # txt_decrypt is the empty string which is necessary to save the original text without cryptography soon
   txt_decrypt = ""
   # key for decrypting the cipher
   key = "ROCKET"
-  # declare it as a character
-  letter_encrypt = chr
 
   # it goes through each letter of the cryptography text
+  # j is to change the letter on the key
   j = 0
   for letter_encrypt in txt_encrypt:
-    # txt_decrypt calls letter_jumpdown method with two arguments
+    # txt_decrypt calls letter_jumpdown method with three arguments
     letter = letter_jumpdown(letter_encrypt, key, j)
     txt_decrypt += letter
     # it is to change j, to be always a letter after another
     j = j + 1
-
-    # it is to the key doesn't stop, if it is over it will start again
+    # it means that if j is over it will start again
     if j >= len(key):
         j = 0
 
-  # print plaintext
+  # print plaintext (the real text that we can understand)
   print("The plaintext is: ", txt_decrypt)
 
 # it is necessary for the interpreter to understand that the program must initially runs here
